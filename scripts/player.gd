4da3935e12d8a308bb746c523b84450dc9a1d134
Nilansh Sharma <80972022+nilanshsharma23@users.noreply.github.com>
2025-10-04 14:28:53 +0530

    strength meter

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,17 @@ var walk_speed: float = 100
 const JUMP_SPEED: float = 200
 
 var is_touching_ground: bool = false
+var filled: bool = false
 
 @export var body_parts: Array[Sprite2D]
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var limbs: Node2D = $Limbs
+@onready var strength_meter: TextureProgressBar = $CanvasLayer/Control/StrengthMeter
+
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("fill_meter"):
+		strength_meter.value += 1
 
 func _physics_process(delta: float) -> void:
 	velocity.y += delta * GRAVITY
@@ -51,6 +57,9 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_touching_ground:
 		velocity.y -= JUMP_SPEED
 	
+	if filled:
+		print("filled")
+	
 	move_and_slide()
 
 func _on_ground_detector_body_entered(_body: Node2D) -> void:
@@ -58,3 +67,9 @@ func _on_ground_detector_body_entered(_body: Node2D) -> void:
 
 func _on_ground_detector_body_exited(_body: Node2D) -> void:
 	is_touching_ground = false
+
+func _on_strength_meter_value_changed(value: float) -> void:
+	if value == 100:
+		filled = true
+	else:
+		filled = false
