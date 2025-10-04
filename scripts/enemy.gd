9e5b1a6805b5efe850c384f19a9944a8a174bc91
adi0909-404 <percygamez0909@gmail.com>
2025-10-04 21:57:39 +0530

    Update enemy.gd

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,22 @@
 extends CharacterBody2D
 
-func _on_hurtbox_hit() -> void:
-	print("here")
+const GRAVITY: float = 400.0
+const SPEED: float = 80.0
+var direction: int = 1  # 1 = right, -1 = left
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var patrol_timer: Timer = $PatrolTimer
+
+func _ready() -> void:
+	patrol_timer.timeout.connect(_on_patrol_timer_timeout)
+	patrol_timer.start()
+
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	velocity.x = SPEED * direction
+	move_and_slide()
+
+	sprite.flip_h = direction < 0
+
+func _on_patrol_timer_timeout() -> void:
+	direction *= -1
